391c7c595d960561cae388f59828a8dbddc35d1d
Brian Whitton <me@brianwhitton.com>
2019-02-08 18:10:35 -0500

    lookup the passed in icon name before passing to template
    prevents dying on a typo

--- addon/components/nypr-svg.js
+++ addon/components/nypr-svg.js
@@ -1,11 +1,14 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { getOwner } from '@ember/application';
 
 export default Component.extend({
   tagName: '',
 
   svgPartial: computed(function() {
-    return this.icon ? `components/nypr-svg/${this.icon}` : false;
+    let owner = getOwner(this);
+    let templateExists = Boolean(owner.lookup(`templates/component:nypr-svg/${this.icon}`));
+    return templateExists ? `components/nypr-svg/${this.icon}` : false;
   })
 }).reopenClass({
   positionalParams: ['icon', 'class'],
