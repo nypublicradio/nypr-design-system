9028df99e333f8eeb696dfdf4e5ede95549348c2
Brian Whitton <me@brianwhitton.com>
2019-02-08 18:13:43 -0500

    add a warning

--- addon/components/nypr-svg.js
+++ addon/components/nypr-svg.js
@@ -2,13 +2,21 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { getOwner } from '@ember/application';
 
+const msg = icon =>
+  `Could not find an icon in nypr-design-system/app/templates/components/nypr-svg named ${icon}`;
+
 export default Component.extend({
   tagName: '',
 
   svgPartial: computed(function() {
     let owner = getOwner(this);
     let templateExists = Boolean(owner.lookup(`templates/component:nypr-svg/${this.icon}`));
-    return templateExists ? `components/nypr-svg/${this.icon}` : false;
+    if (templateExists) {
+      return `components/nypr-svg/${this.icon}`;
+    } else {
+      console.warn(msg(this.icon)); // eslint-disable-line
+      return false;
+    }
   })
 }).reopenClass({
   positionalParams: ['icon', 'class'],
