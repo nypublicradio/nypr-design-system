1451bf247e51eb65f3884275f3327c7f2c285392
Brian Whitton <me@brianwhitton.com>
2019-03-12 15:49:12 -0400

    adds some api docs to nypr-svg

--- addon/components/nypr-svg.js
+++ addon/components/nypr-svg.js
@@ -5,9 +5,37 @@ import { getOwner } from '@ember/application';
 const msg = icon =>
   `Could not find an icon in nypr-design-system/app/templates/components/nypr-svg named ${icon}`;
 
+/**
+  A component that will safely render any svg icon found in the `app/templates/components/nypr-svg` tree.
+
+  If an svg template can't be found, a warning will be printed.
+
+  @class NyprSvg
+*/
 export default Component.extend({
   tagName: '',
 
+  /**
+    Filename of the icon to render.
+
+    @argument icon
+    @type {string}
+  */
+  icon: null,
+
+  /**
+    Classname to be applied to rendered SVG markup.
+
+    @argument class
+    @type {string}
+  */
+  class: null,
+
+  /**
+    Looks up the svg template
+
+    @accessor svgPartial
+   */
   svgPartial: computed(function() {
     let owner = getOwner(this);
     let templateExists = Boolean(owner.lookup(`templates/component:nypr-svg/${this.icon}`));
