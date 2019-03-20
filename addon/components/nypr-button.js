708510b1880e9e45c281b923ef66e60f26445b69
Brian Whitton <me@brianwhitton.com>
2019-03-20 12:17:29 -0400

    add nypr-button docs

--- addon/components/nypr-button.js
+++ addon/components/nypr-button.js
@@ -3,19 +3,59 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 import layout from '../templates/components/nypr-button';
 
+/**
+  Base button. Use this component to compose more specific buttons and components.
+
+  @class NyprButton
+*/
 export default Component.extend({
   layout,
   tagName: 'button',
 
-  classNames: ['nypr-button'],
-  classNameBindings: ['themeClass'],
+  classNames: ['nypr-button', 'o-button'],
+  classNameBindings: ['tierClass'],
 
-  attributeBindings: ['type'],
+  attributeBindings: ['type', 'role'],
 
+  /**
+    Specify the `role` attribute. Default is 'button'.
+    @argument role
+    @type {string}
+  */
+  role: 'button',
+
+  /**
+    Specify the `type` attribute for the button. Default is "button".
+    @argument type
+    @type {string}
+  */
   type: 'button',
 
-  themeClass: computed('theme', function() {
-    return `nypr-button--${this.theme || 'nypr'}`;
-  })
+  /**
+    Which tier in the visual heirarchy this button should be.
+
+    @argument tier
+    @type {string}
+  */
+  tier: null,
+
+
+  /**
+    Computes the class applied based on the value of `tier`
+
+    @accessor tierClass
+    @type {string}
+  */
+  tierClass: computed('tier', function() {
+    return this.tier ? `o-button--${this.tier}` : '';
+  }),
+
+  /**
+    Specify a click handler
+
+    @argument click
+    @type {function}
+  */
+  click() {}
 });
 //END-SNIPPET
