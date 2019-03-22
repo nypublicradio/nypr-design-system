ab16ba74fa2e9ef62236c147667599b96453b4fc
Brian Whitton <me@brianwhitton.com>
2019-03-22 16:39:50 -0400

    api docs for media object

--- addon/components/atoms/media-object.js
+++ addon/components/atoms/media-object.js
@@ -1,8 +1,25 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/atoms/media-object';
 
+/**
+  The OOCSS Media Object [popularized by Nicole Sullivan](http://www.stubbornella.org/content/2010/06/25/the-media-object-saves-hundreds-of-lines-of-code/)
+  @class atoms/media-object
+  @yield {Hash} mo
+  @yield {Component} mo.media - Pass a block for the "media" area
+  @yield {Component} mo.object - Pass a block for the "object" area
+*/
 export default Component.extend({
   layout,
   classNames: ['media-object'],
   classNameBindings: ['valign'],
+
+  /**
+    Specify a vertical alignment.
+    Accepts `'t'`, `'c'`, `'b'` for top, center, and bottom.
+
+    @argument valign
+    @type {String?}
+    @default null
+  */
+  valign: null,
 });
