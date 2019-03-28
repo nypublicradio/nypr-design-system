28b83eaad1ce8229e4b431e7255c6f22b9b94207
Brian Whitton <me@brianwhitton.com>
2019-03-28 18:03:52 -0400

    make this test useful

--- tests/integration/components/nypr-m-block-test.js
+++ tests/integration/components/nypr-m-block-test.js
@@ -7,20 +7,11 @@ module('Integration | Component | nypr-m-block', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<NyprMBlock/>`);
+    assert.dom('.c-block').exists();
 
-    assert.equal(this.element.textContent.trim(), '');
+    await render(hbs`<NyprMBlock @orientation='h'/>`);
+    assert.dom('.c-block--item').exists('`orientation` controls view');
 
-    // Template block usage:
-    await render(hbs`
-      <NyprMBlock>
-        template block text
-      </NyprMBlock>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
