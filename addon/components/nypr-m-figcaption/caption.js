e41e9f5886ee505fb1ac6ee67a464c4592121592
Brian Whitton <me@brianwhitton.com>
2019-04-03 13:41:13 -0400

    add nested contextual components to figcaption with block usage examples
    going to require some updates to pattern lab

---
+++ addon/components/nypr-m-figcaption/caption.js
@@ -0,0 +1,9 @@
+// BEGIN-SNIPPET nypr-m-figcaption-caption.js
+import Component from '@ember/component';
+import layout from '../../templates/components/nypr-m-figcaption/caption';
+
+export default Component.extend({
+  layout,
+  tagName: '',
+});
+// END-SNIPPET
---
+++ addon/components/nypr-m-figcaption/credit.js
@@ -0,0 +1,9 @@
+// BEGIN-SNIPPET nypr-m-figcaption-credit.js
+import Component from '@ember/component';
+import layout from '../../templates/components/nypr-m-figcaption/credit';
+
+export default Component.extend({
+  layout,
+  tagName: '',
+});
+// END-SNIPPET
