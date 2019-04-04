9bd6609d056d6b25a2ae0ac7615716b9cb7a2ac4
Brian Whitton <me@brianwhitton.com>
2019-04-04 08:44:43 -0400

    adds basic to top component

---
+++ addon/components/nypr-m-to-top.js
@@ -0,0 +1,35 @@
+import Component from '@ember/component';
+import layout from '../templates/components/nypr-m-to-top';
+
+export default Component.extend({
+  layout,
+  tagName: '',
+
+  /**
+    Optional dom selector scroll target
+
+    @argument selector
+    @type {String}
+  */
+
+  /**
+    Optional offset from selector target
+
+    @argument offset
+    @type {Number}
+  */
+  offset: 0,
+
+  /**
+    Scrolls window to top of page or provided selector
+
+    @method toTop
+    @return {Void}
+  */
+  toTop() {
+    let offset = this.offset;
+    let target = document.querySelector(this.selector);
+    let y = target ? target.offsetTop - (target.offsetHeight + offset) : 0;
+    window.scrollTo({top: y, behavior: 'smooth'});
+  }
+});
