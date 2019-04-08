3153292307239f2a38e3711f2cdb3acb3df3c143
Brian Whitton <me@brianwhitton.com>
2019-04-08 17:48:36 -0400

    add fixed header handler

--- addon/components/nypr-o-header.js
+++ addon/components/nypr-o-header.js
@@ -1,5 +1,6 @@
 // BEGIN-SNIPPET nypr-o-header.js
 import Component from '@ember/component';
+import { debounce, bind } from '@ember/runloop';
 import layout from '../templates/components/nypr-o-header';
 
 /**
@@ -16,8 +17,32 @@ export default Component.extend({
   layout,
   tagName: 'header',
   classNames: ['c-main-header'],
-  classNameBindings: ['isOpen:side-menu-is-active'],
+  classNameBindings: ['isOpen:side-menu-is-active', 'outOfViewport'],
 
   isOpen: false,
+
+  init() {
+    this._super(...arguments);
+    this._boundListener = bind(this, '_scrollListener');
+    window.addEventListener('scroll', this._boundListener);
+  },
+
+  willDestroy() {
+    this._super(...arguments);
+    window.removeEventListener('scroll', this._boundListener);
+  },
+
+  /**
+    @method _scrollListener
+    @param {EventObject} event
+    @return {Void}
+  */
+  _scrollListener() {
+    debounce(this, () => {
+      let { top, height } = this.element.getBoundingClientRect();
+      this.set('outOfViewport', top + height < 0);
+    }, 150);
+  }
+
 });
 // END-SNIPPET
