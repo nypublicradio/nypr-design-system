7df8f15484edf7b5cae30c91b032a9653e179b1d
Brian Whitton <me@brianwhitton.com>
2019-04-10 10:36:19 -0400

    add ember basic dropdown's SASS files to the allowed import paths

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 
 module.exports = {
   name: require('./package').name,
-  included: function(/* app */) {
+  included: function(app) {
+    let { sassOptions = {} } = app.options;
+    if (!sassOptions.includePaths) {
+      sassOptions.includePaths = [];
+    }
+    sassOptions.includePaths.push('node_modules/ember-basic-dropdown/app/styles');
+    app.options.sassOptions = sassOptions;
+    
     this._super.included.apply(this, arguments);
   },
 };
