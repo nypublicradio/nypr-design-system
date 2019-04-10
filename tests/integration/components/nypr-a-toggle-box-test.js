1850d4867a2c1b2df6ae17a85c9a1661d1f0b7f8
Brian Whitton <me@brianwhitton.com>
2019-04-10 10:11:47 -0400

    ember basic dropdown 2 isn't ready for primetime
    add a basic interactivity test for toggle box

--- tests/integration/components/nypr-a-toggle-box-test.js
+++ tests/integration/components/nypr-a-toggle-box-test.js
@@ -2,25 +2,28 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { clickTrigger } from 'ember-basic-dropdown/test-support/helpers';
+
 
 module('Integration | Component | nypr-a-toggle-box', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{nypr-a-toggle-box}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
     await render(hbs`
-      {{#nypr-a-toggle-box}}
-        template block text
-      {{/nypr-a-toggle-box}}
+      <NyprAToggleBox as |toggle|>
+        <toggle.trigger>
+          <span>Toggle Box Label</span>
+        </toggle.trigger>
+
+        <toggle.dropdown>
+          Dropdown Contents
+        </toggle.dropdown>
+      </NyprAToggleBox>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    // target span b/c there's an icon with assistive text in the label
+    assert.dom('.c-toggle-box__label span').hasText('Toggle Box Label');
+    await clickTrigger();
+    assert.dom('.c-toggle-box__dropdown').hasText('Dropdown Contents');
   });
 });
