922caf65080c35f9d5bc6f302ef15cb57e93126b
Brian Whitton <me@brianwhitton.com>
2019-04-11 12:31:11 -0400

    close menu when clicked outside

--- addon/components/nypr-o-header/menu.js
+++ addon/components/nypr-o-header/menu.js
@@ -1,8 +1,11 @@
 // BEGIN-SNIPPET nypr-o-header-menu.js
+import ClickOutsideMixin from 'ember-click-outside/mixin';
+import { closest } from 'ember-click-outside/utils';
+
 import Component from '@ember/component';
 import layout from '../../templates/components/nypr-o-header/menu';
 
-export default Component.extend({
+export default Component.extend(ClickOutsideMixin, {
   layout,
   classNames: ['c-side-menu'],
 
@@ -19,5 +22,35 @@ export default Component.extend({
     @argument secondaryNav
     @type {Array<Object>}
   */
+
+  /**
+    Function to close menu when user clicks elsewhere in the document
+
+    @argument close
+    @type {Function}
+  **/
+
+  /**
+    Handler called by `ClickOutsideMixin`
+
+    @method clickOutside
+    @param {Event} e
+    @return {void}
+  */
+  clickOutside(e) {
+    if (!closest(e.target, '.c-menu-toggle')) {
+      this.close();
+    }
+  },
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.addClickOutsideListener();
+  },
+
+  willDestroy() {
+    this._super(...arguments);
+    this.removeClickOutsideListener();
+  },
 });
 // END-SNIPPET
