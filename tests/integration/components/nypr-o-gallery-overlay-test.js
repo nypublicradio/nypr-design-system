920dfab1e7237f695b958ed484799918778a347b
Brian Whitton <me@brianwhitton.com>
2019-04-23 15:58:45 -0400

    gallery overlay test

--- tests/integration/components/nypr-o-gallery-overlay-test.js
+++ tests/integration/components/nypr-o-gallery-overlay-test.js
@@ -1,26 +1,93 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, waitUntil } from '@ember/test-helpers';
+import { set } from '@ember/object';
 import hbs from 'htmlbars-inline-precompile';
+
+import { GALLERY as SLIDES } from 'dummy/controllers/docs/molecules/nypr-m-gallery-lead';
+import { inViewport } from 'dummy/helpers/in-viewport';
 
 module('Integration | Component | nypr-o-gallery-overlay', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    const TITLE = 'Foo Gallery';
+    const AD_TEXT = 'Bar Ad';
 
-    await render(hbs`{{nypr-o-gallery-overlay}}`);
+    this.setProperties({
+      SLIDES,
+      TITLE,
+      AD_TEXT,
+    });
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
     await render(hbs`
-      {{#nypr-o-gallery-overlay}}
-        template block text
-      {{/nypr-o-gallery-overlay}}
+      <NyprOGalleryOverlay
+        @title={{TITLE}}
+        @parentRoute='so it renders'
+        @slides={{SLIDES}}
+        as |index gallery|>
+
+        {{#if (eq index 1)}}
+          <gallery.billboard>
+            {{AD_TEXT}}
+          </gallery.billboard>
+        {{/if}}
+
+      </NyprOGalleryOverlay>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('[data-test-gallery-title]').hasText(TITLE);
+    assert.dom('[data-test-gallery-slide]').exists({count: SLIDES.length});
+    assert.dom('[data-test-gallery-billboard]').includesText(AD_TEXT);
+
+    const gallery = document.querySelector('[data-test-gallery-overlay]');
+    assert.dom(document.body).hasStyle({
+      height: `${gallery.getBoundingClientRect().height}px`
+    }, 'resizes body to match gallery height');
   });
+
+  test('it scrolls to a slide at the specified zero-based index on render', async function(assert) {
+    const TITLE = 'Foo Gallery';
+    const ACTIVE_SLIDE = SLIDES.length - 1;
+
+    this.setProperties({
+      SLIDES: SLIDES.map((slide, i) => {
+        set(slide, 'full', `/test-image${i + 1}.jpeg`);
+        return slide;
+      }),
+      TITLE,
+      ACTIVE_SLIDE,
+    });
+
+    await render(hbs`
+      <NyprOGalleryOverlay
+        @title={{TITLE}}
+        @parentRoute='so it renders'
+        @slides={{SLIDES}}
+        @activeSlide={{ACTIVE_SLIDE}}
+      />
+    `);
+
+    const activeSlide = document.querySelector(`[data-test-gallery-slide="${ACTIVE_SLIDE}"]`);
+    // await pauseTest();
+    await waitUntil(() => {
+      return inViewport(activeSlide);
+    }, {timeout: 1000});
+    assert.ok(`slide ${ACTIVE_SLIDE + 1} should be visible`);
+  });
+
+  test('slides call the gallery `registerSlide` method on insert', async function(assert) {
+    this.setProperties({
+      registerSlide: slide => assert.ok(slide instanceof HTMLElement),
+      SLIDES,
+    });
+    assert.expect(SLIDES.length);
+
+    await render(hbs`
+      <NyprOGalleryOverlay
+        @registerSlide={{registerSlide}}
+        @slides={{SLIDES}}
+      />
+    `);
+  })
 });
