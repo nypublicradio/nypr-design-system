d283a0c22a541e53c9922d9e3bb62d72b53fb63c
Brian Whitton <me@brianwhitton.com>
2019-04-29 08:58:05 -0400

    add label component

---
+++ addon/components/nypr-a-ad-label.js
@@ -0,0 +1,15 @@
+// BEGIN-SNIPPET nypr-a-ad-label.js
+import Component from '@ember/component';
+import layout from '../templates/components/nypr-a-ad-label';
+
+/**
+  Advertising label
+
+  @class nypr-a-ad-label
+*/
+export default Component.extend({
+  layout,
+  tagName: 'span',
+  classNames: ['o-ad__label', 'u-font--secondary-style', 'u-font--xxs', 'u-color--gray'],
+});
+// END-SNIPPET
