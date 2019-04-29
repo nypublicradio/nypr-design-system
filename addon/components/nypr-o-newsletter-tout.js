4d4ec84a712f4353a0a2948e13bcc02b224b9fc5
Brian Whitton <me@brianwhitton.com>
2019-04-29 16:08:49 -0400

    adds newlsetter tout docs

--- addon/components/nypr-o-newsletter-tout.js
+++ addon/components/nypr-o-newsletter-tout.js
@@ -1,3 +1,4 @@
+// BEGIN-SNIPPET nypr-o-newsletter-tout.js
 import Component from '@ember/component';
 import layout from '../templates/components/nypr-o-newsletter-tout';
 
@@ -5,3 +6,4 @@ export default Component.extend({
   layout,
   classNames: ['c-newsletter-tout', 'u-reversed-out'],
 });
+// END-SNIPPET
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -50,6 +50,7 @@ Router.map(function() {
       this.route('nypr-o-donate');
       this.route('nypr-o-related-content');
       this.route('nypr-o-sponsored-tout');
+      this.route('nypr-o-newsletter-tout');
     })
   });
 
