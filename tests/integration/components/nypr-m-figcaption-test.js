8d2c060d34438835f57c8eb00e42f998fa6ae645
Brian Whitton <me@brianwhitton.com>
2019-05-14 16:27:32 -0400

    variations on figcaption
    the current data is unreliable so enforce multiple options for rendering 
    the icon

--- tests/integration/components/nypr-m-figcaption-test.js
+++ tests/integration/components/nypr-m-figcaption-test.js
@@ -21,5 +21,46 @@ module('Integration | Component | nypr-m-figcaption', function(hooks) {
     assert.dom('figcaption.o-caption').exists();
     assert.dom('figcaption').includesText(CAPTION);
     assert.dom('figcaption .o-credit').hasText(CREDIT);
+  });
+
+  test('the many variations of figcaptions', async function(assert) {
+    const CAPTION = "Figure caption";
+    const CREDIT = "Credit for this figure";
+    this.setProperties({
+      caption: CAPTION,
+      credit: CREDIT,
+    });
+
+    await render(hbs`
+      <NyprMFigcaption @caption={{caption}} @credit={{credit}}/>
+    `);
+    assert.dom('.o-caption__text > span > span.o-icon').exists('inline form puts the icon insdide an unmarked span.');
+
+    await render(hbs`
+      <NyprMFigcaption @credit={{credit}}/>
+    `);
+    assert.dom('.o-caption__text > span.o-credit > span.o-icon').exists('inline form WITHOUT a caption puts the icon inside the credit span');
+
+    await render(hbs`
+      <NyprMFigcaption as |figcaption|>
+        <figcaption.caption @caption={{caption}} @credit={{credit}} />
+      </NyprMFigcaption>
+    `);
+    assert.dom('.o-caption__text > span > span.o-icon').exists('block form with inline caption puts the icon insdide an unmarked span.');
+
+    await render(hbs`
+      <NyprMFigcaption as |figcaption|>
+        <figcaption.caption @caption={{caption}} />
+        <figcaption.credit @credit={{credit}} />
+      </NyprMFigcaption>
+    `);
+    assert.dom('.o-caption__text > span > span.o-icon').exists('block form with inline caption and inline credit puts the icon inside an unmarked span.');
+
+    await render(hbs`
+      <NyprMFigcaption as |figcaption|>
+        <figcaption.credit @credit={{credit}} @includeIcon={{true}}/>
+      </NyprMFigcaption>
+    `);
+    assert.dom('.o-caption > span.o-credit > span.o-icon').exists('block form with only inline credit requires @includeIcon to show an icon');
   })
 });
