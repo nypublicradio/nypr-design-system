e9ff58c6bf9d56d23faa226fe7da923a7b89949f
Brian Whitton <bwhitton@wnyc.org>
2019-06-04 13:21:53 -0400

    don't need this to have an offset

--- addon/components/nypr-o-header.js
+++ addon/components/nypr-o-header.js
@@ -152,7 +152,7 @@ export default Component.extend({
   setMenuHeightOffset() {
     let header = this.element.querySelector('#header-inner');
     if (header) {
-      this.set('menuHeightOffset', header.offsetTop - window.scrollY);
+      this.set('menuHeightOffset', header.offsetTop);
     }
   },
 
