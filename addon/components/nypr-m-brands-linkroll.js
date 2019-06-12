9cfd3f504c6cc788401205af8f8ae3a9ae3ad407
Brian Whitton <me@brianwhitton.com>
2019-06-12 14:05:33 -0400

    adds optional exclude param to brands linkroll

--- addon/components/nypr-m-brands-linkroll.js
+++ addon/components/nypr-m-brands-linkroll.js
@@ -1,6 +1,7 @@
 // BEGIN-SNIPPET nypr-m-brands-linkroll.js
 import Component from '@ember/component';
 import layout from '../templates/components/nypr-m-brands-linkroll';
+import { computed } from '@ember/object';
 
 import { positionBottomCenter } from '../utils/toggle-box-positions';
 
@@ -10,6 +11,9 @@ const NAV_ITEMS = [{
 }, {
   url: 'https://www.newsounds.org',
   title: 'New Sounds',
+}, {
+  url: 'https://www.wqxr.org',
+  title: 'WQXR',
 }, {
   url: 'https://gothamist.com',
   title: 'Gothamist',
@@ -24,6 +28,11 @@ const NAV_ITEMS = [{
   title: 'The Greene Space',
 }];
 
+/**
+  Extends `nypr-a-toggle-box` to make a flyout with links to NYPR properties.
+
+  @class nypr-m-brands-linkroll
+*/
 export default Component.extend({
   layout,
   tagName: '',
@@ -35,6 +44,32 @@ export default Component.extend({
     @type {Array<Object>}
   */
   navItems: NAV_ITEMS,
+
+  /**
+    Choose a nav item to exclude. Will coerce to lowercase and compare with
+    each objects lowercased `title` key.
+
+    @argument exclude
+    @type {String}
+  */
+
+  items: computed('navItems', 'exclude', function() {
+    let { navItems, exclude } = this;
+    if (!navItems || navItems.length === 0) {
+      return [];
+    }
+
+    if (exclude && typeof exclude === 'string') {
+      return navItems.filter(item => item.title.toLowerCase() !== exclude.toLowerCase());
+    } else if (Array.isArray(exclude)) {
+      exclude = exclude
+        .map(e => typeof e === 'string' && e.toLowerCase())
+        .filter(e => !!e);
+      return navItems.filter(item => !exclude.includes(item.title.toLowerCase()));
+    } else {
+      return navItems;
+    }
+  }),
 
   calculatePosition(trigger, content) {
     return positionBottomCenter(trigger, content, 15);
--- tests/integration/components/nypr-m-brands-linkroll-test.js
+++ tests/integration/components/nypr-m-brands-linkroll-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | nypr-m-brands-linkroll', function(hooks) {
@@ -11,4 +11,19 @@ module('Integration | Component | nypr-m-brands-linkroll', function(hooks) {
 
     assert.dom('.c-toggle-box--linkroll').exists();
   });
+
+  test('can choose a site to exclude', async function(assert) {
+    await render(hbs`<NyprMBrandsLinkroll @exclude='gothamist'/>`);
+
+    await click('.c-toggle-box--linkroll > button'); // open the linkroll
+    assert.dom('.c-nypr-nav__list').doesNotIncludeText('Gothamist');
+  });
+
+  test('can exclude multiple sites', async function(assert) {
+    await render(hbs`<NyprMBrandsLinkroll @exclude={{array 'gothamist' 'wqxr'}}/>`);
+
+    await click('.c-toggle-box--linkroll > button'); // open the linkroll
+    assert.dom('.c-nypr-nav__list').doesNotIncludeText('Gothamist');
+    assert.dom('.c-nypr-nav__list').doesNotIncludeText('WQXR');
+  })
 });
