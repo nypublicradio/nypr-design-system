7e9d6462ff5d1908abacabfac320f5bef5178404
Brian Whitton <me@brianwhitton.com>
2019-06-12 12:36:44 -0400

    Revert "clean up block title."
    This reverts commit 90015ba4962bcf03099ee684c5f3fe4824116a3b.

--- addon/components/nypr-m-block/title-layout.js
+++
@@ -1,8 +0,0 @@
-import Component from '@ember/component';
-import layout from '../../templates/components/nypr-m-block/title-layout';
-
-
-export default Component.extend({
-  layout,
-  tagName: '',
-});
--- addon/components/nypr-m-block/title.js
+++ addon/components/nypr-m-block/title.js
@@ -1,6 +1,4 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { collect } from '@ember/object/computed';
 import layout from '../../templates/components/nypr-m-block/title';
 
 /**
@@ -76,33 +74,4 @@ export default Component.extend({
     @argument hasAudio
     @type {Boolean}
   */
-
-  galleryIconClass: computed('hasGallery', function() {
-    if (this.hasGallery) {
-      return 'c-block__title--has-icon c-block__title--is-gallery';
-    }
-  }),
-
-  videoIconClass: computed('hasVideo', function() {
-    if (this.hasVideo && !this.hasGallery) {
-      return 'c-block__title--has-icon c-block__title--is-video';
-    }
-  }),
-
-  audioIconClass: computed('hasAudio', function() {
-    if (this.hasAudio) {
-      return 'c-block__title--has-icon c-block__title--is-audio';
-    }
-  }),
-
-  externalIconClass: computed('url', function() {
-    if (this.url) {
-      return 'c-block__title--has-icon c-block__title--is-outbound-link';
-    }
-  }),
-
-  iconClasses: collect('galleryIconClass', 'videoIconClass', 'audioIconClass', 'externalIconClass'),
-  iconClassNames: computed('iconClasses', function() {
-    return this.iconClasses.join(' ');
-  }),
 });
