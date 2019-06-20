7a257cb46dfaf0ef20904fa3d71910d6b1a33af1
Brian Whitton <bwhitton@wnyc.org>
2019-06-20 15:40:37 -0400

    adds box banner and nested compnents

---
+++ addon/components/nypr-m-box-banner.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import layout from '../templates/components/nypr-m-box-banner';
+
+export default Component.extend({
+  layout,
+
+  classNames: ['o-box-banner', 'u-spacing', 'o-3d-heading'],
+});
---
+++ addon/components/nypr-m-box-banner/dek.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import layout from '../../templates/components/nypr-m-box-banner/dek';
+
+export default Component.extend({
+  layout,
+  classNames: ['o-box-banner__dek', 'o-rte-text'],
+});
