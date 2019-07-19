3c614de6342fc45617de297935bd6a6b89f11baa
walsh9 <walsh9@gmail.com>
2019-07-19 15:23:39 -0400

    Update box-banner

--- addon/components/nypr-m-box-banner/title.js
+++ addon/components/nypr-m-box-banner/title.js
@@ -5,6 +5,6 @@ import layout from '../../templates/components/nypr-m-box-banner/title';
 export default Component.extend({
   layout,
   tagName: 'h3',
-  classNames: ['o-box-banner__title', 'u-space--top'],
+  classNames: ['o-box-banner__title'],
 });
 // END-SNIPPET
