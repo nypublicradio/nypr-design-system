c80bf2b49a9cf27f4df13a733488aca01b598a76
walsh9 <walsh9@gmail.com>
2019-08-22 14:11:36 -0400

    Update text banner documentation

--- addon/components/nypr-m-text-banner.js
+++ addon/components/nypr-m-text-banner.js
@@ -1,3 +1,4 @@
+// BEGIN-SNIPPET nypr-m-text-banner.js
 import Component from '@ember/component';
 import layout from '../templates/components/nypr-m-text-banner';
 
@@ -9,6 +10,7 @@ import layout from '../templates/components/nypr-m-text-banner';
     <NyprMTextBanner
       @category='Breaking News'
       @url='Link'
+      @route={{array 'article' 'slug'}}
       @title='Story Title'
       @description='Short summary of the story'
     />
@@ -20,4 +22,44 @@ import layout from '../templates/components/nypr-m-text-banner';
 export default Component.extend({
   layout,
   classNames: ['o-text-banner', 'u-spacing--half'],
+
+  /**
+    Category
+
+    @argument category
+    @type {String}
+  */
+
+  /**
+    URL
+    Link to an external page
+    Use this or @route for a link
+
+    @argument url
+    @type {String}
+  */
+
+  /**
+    Route
+    Array of Routing Parameters
+    Use this or @url for a link
+
+    @argument route
+    @type {[String]}
+  */
+
+  /**
+    Title
+
+    @argument title
+    @type {String}
+  */
+
+  /**
+    Description
+
+    @argument description
+    @type {String}
+  */
 });
+// END-SNIPPET
