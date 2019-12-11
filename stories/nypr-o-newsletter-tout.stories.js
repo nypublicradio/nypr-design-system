b6dde54bdb6cce07d6c0242cbcfff0f3101c996d
Kelly Suitor <ksuitor@nypublicradio.org>
2019-12-11 15:09:18 -0500

    Added knobs for Newsletter CTA

--- stories/nypr-o-newsletter-tout.stories.js
+++ stories/nypr-o-newsletter-tout.stories.js
@@ -1,6 +1,10 @@
 import hbs from 'htmlbars-inline-precompile';
+import { text, withKnobs } from '@storybook/addon-knobs';
 
-export default { title: 'nypr-o-newletter-tout' };
+export default { 
+  title: 'nypr-o-newletter-tout',
+  decorators: [withKnobs]
+};
 
 export const NyprONewsletterTout = () => {
   return {
@@ -21,12 +25,12 @@ export const NyprONewsletterTout = () => {
         <tout.legal/>
       </NyprONewsletterTout>`,
     context: {
-        endpoint: "https://api.demo.nypr.digital/opt-in/v1/subscribe/mailchimp",
+        endpoint: text("api endpoint", "https://api.demo.nypr.digital/opt-in/v1/subscribe/mailchimp"),
         params: {list:"8c376c6dff"},
-        heading: "Sign Up for We the Commuters",
-        blurb: "Comprehensive coverage of transportation in our region from WNYC, Gothamist and more.",
+        heading: text("heading", "Sign Up for We the Commuters"),
+        blurb: text("blurb", "Comprehensive coverage of transportation in our region from WNYC, Gothamist and more."),
         img: {
-            src: "http://www.gothamist.com/static-images/wtc.png",
+            src: text("img src", "https://www.gothamist.com/static-images/wtc.png"),
             alt: "We the Commuters"
         }
     }
