0c5774097c956f511e0f0c346263ca39762cf90d
walsh9 <walsh9@gmail.com>
2020-03-25 14:28:59 -0400

    Add font scale widget to typography story

---
+++ stories/00-Foundations/font-styles.js
@@ -0,0 +1,162 @@
+export default [
+  {
+    text: "Header 10",
+    styles: {
+      fontFamily: 'var(--font-family-header)',
+      fontSize: 'var(--font-size-header-10)',
+      fontWeight: 'var(--font-weight-header-10)',
+      lineHeight: 'var(--line-height-header-10)',
+      letterSpacing: 'var(--letter-spacing-header-10)',
+    }
+  },
+  {
+    text: "Header 9",
+    styles: {
+      fontFamily: 'var(--font-family-header)',
+      fontSize: 'var(--font-size-header-9)',
+      fontWeight: 'var(--font-weight-header-9)',
+      lineHeight: 'var(--line-height-header-9)',
+      letterSpacing: 'var(--letter-spacing-header-9)',
+    }
+  },
+  {
+    text: "Header 8",
+    styles: {
+      fontFamily: 'var(--font-family-header)',
+      fontSize: 'var(--font-size-header-8)',
+      fontWeight: 'var(--font-weight-header-8)',
+      lineHeight: 'var(--line-height-header-8)',
+      letterSpacing: 'var(--letter-spacing-header-8)',
+    }
+  },
+  {
+    text: "Header 7",
+    styles: {
+      fontFamily: 'var(--font-family-header)',
+      fontSize: 'var(--font-size-header-7)',
+      fontWeight: 'var(--font-weight-header-7)',
+      lineHeight: 'var(--line-height-header-7)',
+      letterSpacing: 'var(--letter-spacing-header-7)',
+    }
+  },
+  {
+    text: "Header 6",
+    styles: {
+      fontFamily: 'var(--font-family-header)',
+      fontSize: 'var(--font-size-header-6)',
+      fontWeight: 'var(--font-weight-header-6)',
+      lineHeight: 'var(--line-height-header-6)',
+      letterSpacing: 'var(--letter-spacing-header-6)',
+    }
+  },
+  {
+    text: "Header 5",
+    styles: {
+      fontFamily: 'var(--font-family-header)',
+      fontSize: 'var(--font-size-header-5)',
+      fontWeight: 'var(--font-weight-header-5)',
+      lineHeight: 'var(--line-height-header-5)',
+      letterSpacing: 'var(--letter-spacing-header-5)',
+    }
+  },
+  {
+    text: "Header 4",
+    styles: {
+      fontFamily: 'var(--font-family-header)',
+      fontSize: 'var(--font-size-header-4)',
+      fontWeight: 'var(--font-weight-header-4)',
+      lineHeight: 'var(--line-height-header-4)',
+      letterSpacing: 'var(--letter-spacing-header-4)',
+    }
+  },
+  {
+    text: "Header 3",
+    styles: {
+      fontFamily: 'var(--font-family-header)',
+      fontSize: 'var(--font-size-header-3)',
+      fontWeight: 'var(--font-weight-header-3)',
+      lineHeight: 'var(--line-height-header-3)',
+      letterSpacing: 'var(--letter-spacing-header-3)',
+    }
+  },
+  {
+    text: "Header 2",
+    styles: {
+      fontFamily: 'var(--font-family-header)',
+      fontSize: 'var(--font-size-header-2)',
+      fontWeight: 'var(--font-weight-header-2)',
+      lineHeight: 'var(--line-height-header-2)',
+      letterSpacing: 'var(--letter-spacing-header-2)',
+    }
+  },
+  {
+    text: "Header 1",
+    styles: {
+      fontFamily: 'var(--font-family-header)',
+      fontSize: 'var(--font-size-header-1)',
+      fontWeight: 'var(--font-weight-header-1)',
+      lineHeight: 'var(--line-height-header-1)',
+      letterSpacing: 'var(--letter-spacing-header-1)',
+    }
+  },
+  {
+    text: "Body 2",
+    styles: {
+      fontFamily: 'var(--font-family-body)',
+      fontSize: 'var(--font-size-body-2)',
+      fontWeight: 'var(--font-weight-body-2)',
+      lineHeight: 'var(--line-height-body-2)',
+      letterSpacing: 'var(--letter-spacing-body-2)',
+    }
+  },
+  {
+    text: "Body 1",
+    styles: {
+      fontFamily: 'var(--font-family-body)',
+      fontSize: 'var(--font-size-body-1)',
+      fontWeight: 'var(--font-weight-body-1)',
+      lineHeight: 'var(--line-height-body-1)',
+      letterSpacing: 'var(--letter-spacing-body-1)',
+    }
+  },
+  {
+    text: "Small 4",
+    styles: {
+      fontFamily: 'var(--font-family-small)',
+      fontSize: 'var(--font-size-small-4)',
+      fontWeight: 'var(--font-weight-small-4)',
+      lineHeight: 'var(--line-height-small-4)',
+      letterSpacing: 'var(--letter-spacing-small-4)',
+    }
+  },
+  {
+    text: "Small 3",
+    styles: {
+      fontFamily: 'var(--font-family-small)',
+      fontSize: 'var(--font-size-small-3)',
+      fontWeight: 'var(--font-weight-small-3)',
+      lineHeight: 'var(--line-height-small-3)',
+      letterSpacing: 'var(--letter-spacing-small-3)',
+    }
+  },
+  {
+    text: "Small 2",
+    styles: {
+      fontFamily: 'var(--font-family-small)',
+      fontSize: 'var(--font-size-small-2)',
+      fontWeight: 'var(--font-weight-small-2)',
+      lineHeight: 'var(--line-height-small-2)',
+      letterSpacing: 'var(--letter-spacing-small-2)',
+    }
+  },
+  {
+    text: "Small 1",
+    styles: {
+      fontFamily: 'var(--font-family-small)',
+      fontSize: 'var(--font-size-small-1)',
+      fontWeight: 'var(--font-weight-small-1)',
+      lineHeight: 'var(--line-height-small-1)',
+      letterSpacing: 'var(--letter-spacing-small-1)',
+    }
+  },
+];
