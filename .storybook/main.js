e0e3514b8fc330ebdf50e9c3ff40681c1b2dd96c
Brad Kammin <bkammin@nypublicradio.org>
2020-03-30 14:16:23 -0400

    DS-522 add storysource add-on

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-a11y/register',
+    '@storybook/addon-storysource',
     {
       name: '@storybook/addon-docs',
       options: {
