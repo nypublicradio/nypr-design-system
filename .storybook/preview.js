b7bc0abc6b1b44e427060288780959034c7b5ce4
walsh9 <walsh9@gmail.com>
2020-04-24 17:38:57 -0400

    Move storybook style imports to preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,7 @@ import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 import { setJSONDoc } from '@storybook/addon-docs/ember';
 import docJson from '../dist/storybook-docgen/index.json';
 import { withA11y } from '@storybook/addon-a11y';
+import './storybook-styles.css';
 
 addParameters({
   docs: {
