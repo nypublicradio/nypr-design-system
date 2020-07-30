9b3544d9eada7e6fd7adf9a4c4ca5f053d0f9aed
Kim LaRocca <kim@kimlarocca.com>
2020-07-29 23:02:44 -0400

    finishing it up

--- tests/integration/components/footer-test.js
+++ tests/integration/components/footer-test.js
@@ -10,17 +10,32 @@ module('Integration | Component | footer', function(hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{footer}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
     await render(hbs`
-      {{#footer}}
-        template block text
-      {{/footer}}
+    <Footer as |footer|>
+      <footer.logo>
+        <Icon @icon='wnyc/logo' @title='logo' />
+      </footer.logo>
+
+      <footer.slogan>
+        <p>
+          Listener-supported WNYC is the home for independent journalism and courageous conversation on air and online. Broadcasting live from New York City on 93.9 FM and AM 820 and available online and on the go.
+        </p>
+      </footer.slogan>
+
+      <footer.social>
+        <NyprMShareTools @label='Social text here!' as |share|>
+          <share.link @service='facebook' @username='WNYC'/>
+          <share.link @service='twitter' @username='WNYC' />
+          <share.link @service='instagram' @username='WNYC' />
+        </NyprMShareTools>
+      </footer.social>
+
+      <footer.primaryNav @navItems={{model.primaryNav}}/>
+      <footer.secondaryNav @subheader="Subheader 1" @navItems={{model.secondaryNav}}/>
+      <footer.tertiaryNav @subheader="Subheader 2" @navItems={{model.secondaryNav}}/>
+    </Footer>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('.c-main-footer').exists({count: 1});
   });
 });
