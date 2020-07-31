7c5604c375c5b2333eecaf12dc878a78acc1e9d9
Brad Kammin <bkammin@nypublicradio.org>
2020-07-31 07:20:23 -0400

    DS-906 tests passing

--- tests/integration/components/nypr-o-main-audio-player-test.js
+++ tests/integration/components/nypr-o-main-audio-player-test.js
@@ -1,26 +1,24 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
+import Service from '@ember/service';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | nypr-o-main-audio-player', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    const DataLayerService = Service.extend({
+    });
+    this.owner.register('service:dataLayer', DataLayerService);
+    const HifiService = Service.extend({
+    });
+    this.owner.register('service:hifi', HifiService);
+    const DjService = Service.extend({
+    });
+    this.owner.register('service:dj', DjService);
 
     await render(hbs`{{nypr-o-main-audio-player}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#nypr-o-main-audio-player}}
-        template block text
-      {{/nypr-o-main-audio-player}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('.on-air-now-label-text').hasText('live');
   });
 });
