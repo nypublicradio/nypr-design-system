5175b39e4d545ed1f03f9a57f000da44607c0aef
walsh9 <walsh9@gmail.com>
2020-08-12 17:56:29 -0400

    Upgrade ember to 3.12

--- testem.js
+++ testem.js
@@ -13,6 +13,7 @@ module.exports = {
         // --no-sandbox is needed when running Chrome inside a container
         process.env.CI ? '--no-sandbox' : null,
         '--headless',
+        '--disable-dev-shm-usage',
         '--disable-software-rasterizer',
         '--mute-audio',
         '--remote-debugging-port=0',
