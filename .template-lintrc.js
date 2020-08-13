b59bcb3d5f8edfcefd1ffb20f3f89a75f220d6aa
walsh9 <walsh9@gmail.com>
2020-08-12 21:29:13 -0400

    Upgrade ember to 3.15

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,11 +1,5 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended',
-
-  rules: {
-    quotes: false,
-    'simple-unless': false,
-    'no-triple-curlies': false,
-  }
+  extends: 'octane'
 };
