f394aa01bbfd385aabce5522945873b45cbfacc9
walsh9 <walsh9@gmail.com>
2020-08-14 20:02:10 -0400

    remove this test for now

--- tests/integration/components/button/component-test.js
+++ tests/integration/components/button/component-test.js
@@ -6,21 +6,5 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | button', function(hooks) {
   setupRenderingTest(hooks);
 
-  skip('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<Button>`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <Button>
-        template block text
-      </Button>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
