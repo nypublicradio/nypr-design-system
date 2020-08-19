3e8d700f19a2c25bd387d65c4b4e93dfde8da876
walsh9 <walsh9@gmail.com>
2020-08-18 23:11:50 -0400

    Update button styles, add tests, documentation

--- addon/components/button/component.js
+++ addon/components/button/component.js
@@ -1,7 +1,58 @@
 import Component from '@ember/component';
 import layout from './template';
 
+
+/**
+  A simple button. Use this standalone or as part of more complex forms and
+  elements.
+
+  If all you want is a link that looks like a button, you can provide a url
+  as the "href" property, and it will generate markup with an "a" tag
+  instead of a "button" tag.
+
+  Usage:
+  ```
+  <Button @label="Donate" />
+  ```
+
+  @class button
+*/
 export default Component.extend({
   layout,
   tagName: '',
+
+  /**
+  The text label for the button.
+
+  @argument label
+  @type {String}
+  */
+  label: '',
+
+  /**
+  The name of an icon for the button.
+
+  @argument icon
+  @type {String}
+  */
+  icon: '',
+
+  /**
+  Alternate text for screenreaders. If you only provide and icon and no label,
+  you need to include this.
+
+  @argument alt
+  @type {String}
+  */
+  alt: '',
+
+  /**
+  Provide a url to link to. This is for when you really want a link that looks
+  like a button. Will render an `a` tag that has the same button styling. Will
+  automatically add `target="_blank"` and `rel="noopener"`
+
+  @argument href
+  @type {String}
+  */
+  href: '',
 });
--- tests/integration/components/button/component-test.js
+++ tests/integration/components/button/component-test.js
@@ -1,26 +1,75 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import a11yAudit from 'ember-a11y-testing/test-support/audit';
 
 module('Integration | Component | button', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders a button with a label and an icon', async function(assert) {
+    const BUTTON_LABEL = 'Button';
+    const BUTTON_ICON = 'audio';
+    this.set('label', BUTTON_LABEL);
+    this.set('icon', BUTTON_ICON);
 
-    await render(hbs`<Button />`);
+    await render(hbs`<Button @label={{label}} @icon={{icon}} />`);
 
-    assert.equal(this.element.textContent.trim(), '');
 
-    // Template block usage:
-    await render(hbs`
-      <Button>
-        template block text
-      </Button>
-    `);
+    assert.dom('.button-label').exists();
+    assert.dom('.button-label').hasText(BUTTON_LABEL);
+    assert.dom('svg.button-icon').exists();
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+
+    a11yAudit(this.element).then(() => {
+      assert.ok(true, 'no a11y errors found!');
+    });
+  });
+
+  test('it renders a button with a label only', async function(assert) {
+    const BUTTON_LABEL = 'Button';
+    this.set('label', BUTTON_LABEL);
+
+    await render(hbs`<Button @label={{label}} />`);
+
+    assert.dom('.button-label').exists();
+    assert.dom('.button-label').hasText(BUTTON_LABEL);
+    assert.dom('svg.button-icon').doesNotExist();
+    a11yAudit(this.element).then(() => {
+      assert.ok(true, 'no a11y errors found!');
+    });
+  });
+
+  test('it renders a button with an icon only', async function(assert) {
+    const BUTTON_ICON = 'audio';
+    const ALT_TEXT = 'Play'
+    this.set('icon', BUTTON_ICON);
+    this.set('iconAlt', ALT_TEXT);
+
+    await render(hbs`<Button @icon={{icon}} @iconAlt={{iconAlt}} />`);
+
+    assert.dom('.button-label').doesNotExist();
+    assert.dom('svg.button-icon').exists();
+    assert.dom('.button').hasClass('mod-icon-only');
+    assert.dom('.button-icon title').hasText(ALT_TEXT);
+    a11yAudit(this.element).then(() => {
+      assert.ok(true, 'no a11y errors found!');
+    });
+  });
+
+  test('the onClick event works', async function(assert) {
+    assert.expect(1);
+
+    let clickHandler = function() {
+      assert.ok('click handler was triggered')
+    };
+
+    this.set('clickHandler', clickHandler);
+
+    await render(hbs`<Button onClick={{clickHandler}} />`);
+
+    await click('.button');
   });
 });
+
+
