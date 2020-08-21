53c93872d6a5af4aa475f74687a0d411cce62b78
Brad Kammin <bkammin@nypublicradio.org>
2020-08-21 10:56:29 -0400

    DS-923 re-add test

--- tests/integration/components/nypr-o-persistent-audio-player-test.js
+++ tests/integration/components/nypr-o-persistent-audio-player-test.js
@@ -8,17 +8,17 @@ module('Integration | Component | nypr-o-persistent-audio-player', function(hook
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // const DataLayerService = Service.extend({
-    // });
-    // this.owner.register('service:dataLayer', DataLayerService);
-    // const HifiService = Service.extend({
-    // });
-    // this.owner.register('service:hifi', HifiService);
-    // const DjService = Service.extend({
-    // });
-    // this.owner.register('service:dj', DjService);
+    const DataLayerService = Service.extend({
+    });
+    this.owner.register('service:dataLayer', DataLayerService);
+    const HifiService = Service.extend({
+    });
+    this.owner.register('service:hifi', HifiService);
+    const DjService = Service.extend({
+    });
+    this.owner.register('service:dj', DjService);
 
-    // await render(hbs`{{nypr-o-persistent-audio-player}}`);
-    // assert.dom('.nypr-o-audio-player-play-button').exists();
+    await render(hbs`{{nypr-o-persistent-audio-player}}`);
+    assert.dom('.nypr-o-audio-player-play-button').exists();
   });
 });
