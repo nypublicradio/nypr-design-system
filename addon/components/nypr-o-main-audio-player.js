95fceaba8fa4edccab76c673a05d8ab638091083
Brad Kammin <bkammin@nypublicradio.org>
2020-08-22 09:47:33 -0400

    WNYC-163 live, show image, title, description, listen button

--- addon/components/nypr-o-main-audio-player.js
+++ addon/components/nypr-o-main-audio-player.js
@@ -3,6 +3,7 @@ import layout from '../templates/components/nypr-o-main-audio-player';
 
 export default Component.extend({
   layout,
+  classNames: ['nypr-o-main-audio-player'],
 
   actions: {
     openTwitterMention() {
