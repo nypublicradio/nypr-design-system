7e3cae9c19211f832225d155187e3743298a53bd
Brad Kammin <bkammin@nypublicradio.org>
2020-08-28 12:29:33 -0400

    WNYC-163 disable lint for angle bracket expansion warning

--- addon/components/organism/whats-on/component.js
+++ addon/components/organism/whats-on/component.js
@@ -6,7 +6,7 @@ export default Component.extend({
   layout,
   classNames: ['whats-on'],
 
-  displayEpisode: computed('nowPlaying.stream.currentShow.description', 'nowPlaying.show.tease', function() {
+  displayEpisode: computed('nowPlaying.stream.currentShow.description', 'nowPlaying.show.tease', function() { // eslint-disable-line
     // todo: if segments exist, then true
     // todo: refactor when we clean up data/api
     return get(this, 'nowPlaying.stream.currentShow.group_slug') == get(this, 'nowPlaying.show.slug') &&
