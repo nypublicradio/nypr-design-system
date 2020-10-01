b74fb5dd452178d8f900ff6762efeefe453167e7
Kim LaRocca <kim@kimlarocca.com>
2020-10-01 10:25:12 -0400

    update on air image to be clickable

--- addon/components/nypr-m-on-air-image.js
+++ addon/components/nypr-m-on-air-image.js
@@ -1,7 +1,28 @@
 import Component from '@ember/component';
+import { inject as service } from "@ember/service";
 import layout from '../templates/components/nypr-m-on-air-image';
+import { reads } from "@ember/object/computed";
 
 export default Component.extend({
   layout,
-  classNames: ['on-air-image']
+  dataLayer: service(),
+  playerLabel: 'persistent', // default
+  classNames: ['on-air-image'],
+
+  hifi                : service(),
+  dj                  : service(),
+  isReady             : reads('hifi.isReady'),
+  isPlaying           : reads('hifi.isPlaying'),
+  isLoading           : reads('hifi.isLoading'),
+  isStream            : reads('hifi.isStream'),
+
+  handleClick() {
+    if (this.isPlaying) {
+      this.hifi.pause();
+    }
+    else {
+      this.dj.play(this.playItemId);
+    }
+    this.dataLayer.push({ playerSource: this.playerLabel });
+  },
 });
