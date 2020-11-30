e5b056c5d71b5f2367567bad9516237d251fba05
walsh9 <walsh9@gmail.com>
2020-11-30 11:43:51 -0500

    Fix syntax on a fastboot check

--- addon/helpers/can-change-volume.js
+++ addon/helpers/can-change-volume.js
@@ -4,7 +4,7 @@ import { helper } from '@ember/component/helper';
 // https://developer.apple.com/library/content/documentation/AudioVideo/Conceptual/Using_HTML5_Audio_Video/Device-SpecificConsiderations/Device-SpecificConsiderations.html#//apple_ref/doc/uid/TP40009523-CH5-SW11
 
 export function canChangeVolume() {
-  if (document && document.createElement) {
+  if (typeof(document) !== 'undefined' && document.createElement) {
     let audio = document.createElement('audio');
     if (audio.volume !== undefined) {
       audio.volume = 0.99; //arbitrary value
